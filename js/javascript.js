ff1943ab6802b409b0ae6eb8de6d1f99cea9f929
Martin Buchan <martinbuchan92@gmail.com>
2017-10-03 23:04:13 +1100

    Moved to folders for new hosting fixed links to be relative

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+// Martin Buchan
+// 3/10/17
+// Personal Website
+
+"use strict";
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
